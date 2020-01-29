2897cd352c632564fc7af4a09b14dcb5bff0ef0f
shaha <sy4172@bs.amalnet.k12.il>
2020-01-29 22:24:59 +0200

    the 3rd app

---
+++ app/src/main/java/com/example/myex003/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.myex003;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    int i;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btn);
+        i = 0;
+    }
+
+    public void count(View view) {
+
+        i++;
+        btn.setText("This is a click number:"+i);
+        if(i > 6) {
+            btn.setText("Enough to click. Go to new start!");
+            i = 0;
+        }
+        }
+    }
+
